e67f6e833a960cd0be6d5d0f043d1213d7997dd5
Stanley42442 <benjamingillespie290@gmail.com>
2026-03-15 17:09:26 +0100

    changes from antigravity

---
+++ android/app/src/main/java/app/lovable/af4b139c57f44e7ab8682181153aa5d9/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.af4b139c57f44e7ab8682181153aa5d9;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.af4b139c57f44e7ab8682181153aa5d9',
-  appName: 'taxforgeng',
+  appId: 'app.taxforgeng.com',
+  appName: 'TaxForGeng',
   webDir: 'dist',
-  server: {
-    url: 'https://af4b139c-57f4-4e7a-b868-2181153aa5d9.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
 };
 
 export default config;
